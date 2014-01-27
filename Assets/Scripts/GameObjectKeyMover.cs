c9a6b318ffdd8aa008c94da880260159cbfb5ee6
DreamKnightKr <dreamknightkr@gmail.com>
2014-01-27 15:47:22 +0900

    Step 1 완료.

---
+++ Assets/Scripts/GameObjectKeyMover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectKeyMover : MonoBehaviour {
+	Transform trans;
+
+	public float moveSpeed = 2f;
+	public float turnSpeed = 90f;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.name += "+Script";
+
+		trans = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//trans 말고 이미 제공하는 변수인 transform을 사용해도 됨.
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		trans.Translate(trans.forward * v * moveSpeed * Time.deltaTime);
+		trans.Rotate( new Vector3(0f, Time.deltaTime * turnSpeed * h, 0f) );
+	}
+}
