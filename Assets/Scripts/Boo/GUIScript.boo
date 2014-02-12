07c9df81641f05ed3658f75fbee43be686f05a16
DreamKnightMac <dreamknightkr@gmail.com>
2014-02-13 04:39:44 +0900

    Resource & StreamingAsset Foloder Load 구현(Boo)

--- Assets/Scripts/Boo/GUIScript.boo
+++ Assets/Scripts/Boo/GUIScript.boo
@@ -1,33 +1,77 @@
 ﻿import UnityEngine
+import System.Collections
 
 class GUIScript (MonoBehaviour):
 	public objTextHello_Window as GameObject
 	public objTextHello_iOS as GameObject
 	public objTextHello_Android as GameObject
-		
+
+	txt as TextMesh = null
+	www as WWW = null
+
 	def Start ():
 		pass
-	
+
 	def Update ():
 		pass
-		
+
 	def OnGUI() as void:
-    	fPosX as double = 250
-    	fPosY as double = 50
-    	fYInterval as double = 4
-    	nYPosCount as int = 10
-    	
-    	if GUI.Button(Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Show Hello ~"):
-    		// !! Boo currently supports only the ifdef directive. 
-    		// [Unity Doc URL]
-    		//	https://docs.unity3d.com/Documentation/Manual/PlatformDependentCompilation.html
-    		ifdef UNITY_IPHONE:
-            	GameObject.Instantiate( objTextHello_iOS )
-            ifdef UNITY_ANDROID:
-            	GameObject.Instantiate( objTextHello_Android )
-            ifdef UNITY_STANDALONE_WIN:
-            	GameObject.Instantiate( objTextHello_Window )
-            	
-        if GUI.Button(Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Show Resources"):
-        	pass
-            	
+		fPosX as double = 250
+		fPosY as double = 50
+		fYInterval as double = 40
+		nYPosCount as int = 0
+
+		if GUI.Button(Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Show Hello ~"):
+			// !! Boo currently supports only the ifdef directive. 
+			// [Unity Doc URL]
+			//	https://docs.unity3d.com/Documentation/Manual/PlatformDependentCompilation.html
+			if RuntimePlatform.Android == Application.platform :
+				GameObject.Instantiate( objTextHello_iOS )
+			elif RuntimePlatform.Android == Application.platform :
+				GameObject.Instantiate( objTextHello_Android )
+			else :
+				GameObject.Instantiate( objTextHello_Window )
+
+		// Load From Resource
+		nYPosCount++
+		if GUI.Button(Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "(1)Show Resources"):
+			if null == txt :  //  중복 생성 막자...
+				txt = (GameObject.Instantiate( Resources.Load("Prefabs/text") ) as GameObject).GetComponent("TextMesh") as TextMesh
+
+			//textRes as object = Resources.Load("SampleText")
+			textRes as object = Resources.Load("SampleTextCSV")
+			//textRes as object = Resources.Load("SampleTextXML")
+			if textRes isa TextAsset :
+				str as TextAsset = textRes as TextAsset
+				txt.text = str.text
+			else :
+				Debug.LogError("Can't find Text Type Asset")
+
+		// Load From SteamingAssets
+		nYPosCount++;
+		if GUI.Button(Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "(2)Show StreamingAssets"):
+			if null == txt:
+				Debug.LogError ("Press '(1)Show Resources' Button")
+			url as string = Application.streamingAssetsPath + "/TestPage.html"
+			if Application.platform != RuntimePlatform.Android:
+				url = "file://" + url
+			Debug.Log("Get from : " + url)
+			
+			// 자세한 구현 내용은 "Coroutine, 추가 다운로드 Step에 ..."
+			// 중요한건 Resource Folder 보다 복잡하다는 것.. 즉시성이 없다는 것.
+			www = WWW(url);
+			StartCoroutine(LoadAsset())
+	
+	def LoadAsset() as IEnumerator:
+		while not www.isDone:
+			yield WaitForSeconds(0.1f)
+			
+		if null != www.error:
+			Debug.LogError("Can't Load from StreamAsset!! \n" + www.error)
+		else:
+			// Byte Stream -> String
+			txt.text = System.Text.Encoding.UTF8.GetString(www.bytes)
+			Debug.Log("Load Done")
+
+		yield null
+		
--- Assets/Scripts/CShap/GUIScript.cs
+++ Assets/Scripts/CShap/GUIScript.cs
@@ -1,93 +1,88 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿	using UnityEngine;
+	using System.Collections;
 
-public class GUIScript : MonoBehaviour
-{
-    public GameObject objTextHello_Window;
-    public GameObject objTextHello_iOS;
-    public GameObject objTextHello_Android;
+	public class GUIScript : MonoBehaviour
+	{
+			public GameObject objTextHello_Window;
+			public GameObject objTextHello_iOS;
+			public GameObject objTextHello_Android;
+			TextMesh txt = null;
+			WWW www = null;
 
-    TextMesh txt = null;
-    WWW www = null;
+			// Use this for initialization
+			void Start ()
+			{
 
-    // Use this for initialization
-    void Start()
-    {
+			}
+	                
+			// Update is called once per frame
+			void Update ()
+			{
+			}
 
-    }
-                
-    // Update is called once per frame
-    void Update()
-    {
-    }
+			void OnGUI ()
+			{
+					float fPosX = 250, fPosY = 50, fYInterval = 40;
+					int nYPosCount = 0;
 
-    void OnGUI()
-    {
-        float fPosX = 250, fPosY = 50, fYInterval = 40;
-        int nYPosCount = 0;
+					if (GUI.Button (new Rect (fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Show Hello ~")) {
+							#if UNITY_IPHONE
+	            GameObject.Instantiate( objTextHello_iOS );
+							#elif UNITY_ANDROID
+	            GameObject.Instantiate( objTextHello_Android );
+							#else
+							GameObject.Instantiate (objTextHello_Window);
+							#endif
+					}
 
-        if (GUI.Button(new Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Show Hello ~"))
-        {
-            #if UNITY_IPHONE
-            GameObject.Instantiate( objTextHello_iOS );
-            #elif UNITY_ANDROID
-            GameObject.Instantiate( objTextHello_Android );
-            #else
-            GameObject.Instantiate( objTextHello_Window );
-            #endif
-        }
+					// Load From Resource
+					nYPosCount++;
+					if (GUI.Button (new Rect (fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "(1)Show Resources")) {
+							if (null == txt)  //  중복 생성 막자...
+									txt = ((GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/text"))).GetComponent<TextMesh> ();
 
-        // Load From Resource
-        nYPosCount++;
-        if (GUI.Button(new Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Show Resources"))
-        {
-            if(null == txt)  //  중복 생성 막자...
-                txt = ((GameObject)GameObject.Instantiate( Resources.Load("Prefabs/text"))).GetComponent<TextMesh>();
+							//object textRes = Resources.Load("SampleText");
+							object textRes = Resources.Load ("SampleTextCSV");
+							//object textRes = Resources.Load("SampleTextXML");
+							if (textRes is TextAsset) {
+									TextAsset str = textRes as TextAsset;
+									txt.text = str.text;
+							} else
+									Debug.LogError ("Can't find Text Type Asset");
+					}
 
-            //object textRes = Resources.Load("SampleText");
-            object textRes = Resources.Load("SampleTextCSV");
-            //object textRes = Resources.Load("SampleTextXML");
-            if(textRes is TextAsset)
-            {
-                TextAsset str = textRes as TextAsset;
-                txt.text = str.text;
-            }
-            else
-                Debug.LogError("Can't find Text Type Asset");
-        }
+					// Load From SteamingAssets
+					nYPosCount++;
+					if (GUI.Button (new Rect (fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "(2)Show StreamingAssets")) {
+						if (null == txt)
+							Debug.LogError ("Press '(1)Show Resources' Button");
 
-        // Load From SteamingAssets
-        nYPosCount++;
-        if (GUI.Button(new Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Show StreamingAssets"))
-        {
-            string url = Application.streamingAssetsPath + "/TestPage.html";
-            if(Application.platform != RuntimePlatform.Android)
-                url = "file://" + url;
-            Debug.Log("Get from : " + url);
+							string url = Application.streamingAssetsPath + "/TestPage.html";
+							if (Application.platform != RuntimePlatform.Android)
+									url = "file://" + url;
+							Debug.Log ("Get from : " + url);
 
-            // 자세한 구현 내용은 "Corutine, 추가 다운로드 Step에 ..."
-            // 중요한건 Resource Folder 보다 복잡하다는 것.. 즉시성이 없다는 것.
-            www = new WWW(url);
-            StartCoroutine(LoadAsset());
-        }
-    }
+							// 자세한 구현 내용은 "Coroutine, 추가 다운로드 Step에 ..."
+							// 중요한건 Resource Folder 보다 복잡하다는 것.. 즉시성이 없다는 것.
+							www = new WWW (url);
+							StartCoroutine (LoadAsset ());
+					}
+			}
 
-    IEnumerator LoadAsset()
-    {
-        while(!www.isDone)
-        {
-            yield return new WaitForSeconds(0.1f);
-        }
+			IEnumerator LoadAsset ()
+			{
+					while (!www.isDone) {
+							yield return new WaitForSeconds (0.1f);
+					}
 
-        if(null != www.error)
-            Debug.LogError("Can't Load from StreamAsset!! \n" + www.error);
-        else
-        {
-            // Byte Stream -> String
-            txt.text = System.Text.Encoding.UTF8.GetString(www.bytes);
-            Debug.Log("Load Done");
-        }
+					if (null != www.error)
+							Debug.LogError ("Can't Load from StreamAsset!! \n" + www.error);
+					else {
+							// Byte Stream -> String
+							txt.text = System.Text.Encoding.UTF8.GetString (www.bytes);
+							Debug.Log ("Load Done");
+					}
 
-        yield return null;
-    }
-}
+					yield return null;
+			}
+	}
